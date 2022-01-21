9e613d95174510539fc85b433665e7d997456237
Aakanksha Patwa <aakankshapatwa36@gmail.com>
2022-01-21 20:01:21 +0530

    Create Scaffold of Cart,lineitem and add add to cart feature

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,6 +2,8 @@ class Instrument < ApplicationRecord
 	# mount_uploader :image, ImageUploader
 	has_one_attached :image
 	serialize :image, JSON
+	has_many :line_items
+	before_destroy :not_referenced_by_any_line_item
 	belongs_to :user
 
 	validates :title, :brand, :price, :model, presence: true
@@ -12,5 +14,14 @@ class Instrument < ApplicationRecord
     BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
     FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
     CONDITION = %w{ New Excellent Mint Used Fair Poor }
+
+    private
+
+    def not_referenced_by_any_line_item
+    	unless line_items.empty?
+    		errors.add(:base, "Line items present")
+    		throw :abort
+    	end
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_121313) do
+ActiveRecord::Schema.define(version: 2022_01_21_111345) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2022_01_20_121313) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
     t.string "model"
@@ -45,6 +50,16 @@ ActiveRecord::Schema.define(version: 2022_01_20_121313) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "user_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "instrument_id"
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["instrument_id"], name: "index_line_items_on_instrument_id"
   end
 
   create_table "users", force: :cascade do |t|
